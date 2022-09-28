a29629523a4e440b544a9ed17fcc1886e077e4fd
benbinben <benbinben>
2022-09-29 05:12:07 +0700

    clean unused value

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -68,7 +68,6 @@ export default {
     return {
       searchInput: "",
       newline: "\n",
-      animalList: [{ title: "Tiger", date: "11/10/63" }], //****************************
       delList:[{
         // animalID : null,
         boneId : null,
@@ -76,283 +75,7 @@ export default {
       }
       ],
       // animalNameGet: null,
-      animalGet: 
-      [ //Get ข้อมูลจาก API มาใส่ในนี้
-        // {
-        //   _id: "6038af2bff0829223c34adaa",
-        //   thaiName: "ชิมแพนซี",
-        //   englishName: "Chimpanzee",
-        // },
-        // {
-        //   _id: "6038b0ecff0829223c34adab",
-        //   thaiName: "สุนัขบ้าน",
-        //   englishName: "Domestic Dog",
-        // },
-        // {
-        //   _id: "6038b212ff0829223c34adac",
-        //   thaiName: "ลิงแสม",
-        //   englishName: "Crab-eating Macaque",
-        // },
-        // {
-        //   _id: "6038b2d8ff0829223c34adad",
-        //   thaiName: "จิงโจ้แดง",
-        //   englishName: "Red Kangaroo",
-        // },
-        // {
-        //   _id: "6038b34eff0829223c34adae",
-        //   thaiName: "วัลลาบี",
-        //   englishName: "Wallaby",
-        // },
-        // {
-        //   _id: "6038b4b2ff0829223c34adaf",
-        //   thaiName: "แพะบ้าน",
-        //   englishName: "Domestic Goat",
-        // },
-        // {
-        //   _id: "6038b7f0ff0829223c34adb0",
-        //   thaiName: "กระบือปลัก",
-        //   englishName: "Swamp Buffalo",
-        // },
-      ],
-      GetFromId: 
-      { //ข้อมูลกรณียังไม่เคย tag , มี Complete Image,  มี Bone 
-        completeImageLink: [
-          "http://localhost:3000/img/animals/img-1616171184793.jpg",
-          "http://localhost:3000/img/animals/img-1616171184842.jpg",
-          "http://localhost:3000/img/animals/img-1616171184878.jpg",
-          "http://localhost:3000/img/animals/img-1616171184913.jpg",
-          "http://localhost:3000/img/animals/img-1616171184934.jpg",
-          "http://localhost:3000/img/animals/img-1616171184966.jpg",
-          "http://localhost:3000/img/animals/img-1616171185001.jpg",
-          "http://localhost:3000/img/animals/img-1616171185026.jpg",
-          "http://localhost:3000/img/animals/img-1616171185077.jpg",
-          "http://localhost:3000/img/animals/img-1616171185096.jpg",
-          "http://localhost:3000/img/animals/img-1616171185132.jpg",
-          "http://localhost:3000/img/animals/img-1616171185154.jpg",
-          "http://localhost:3000/img/animals/img-1616171185199.jpg",
-          "http://localhost:3000/img/animals/img-1616171185230.jpg",
-          "http://localhost:3000/img/animals/img-1616171185249.jpg",
-          "http://localhost:3000/img/animals/img-1616171185276.jpg",
-        ],
-        _id: "6038b34eff0829223c34adae",
-        bone2DPath: [],
-        picturePath: [],
-        thaiName: "วัลลาบี",
-        englishName: "Wallaby",
-        scienceName: "Macropus rufogriseus",
-        technicalTerm: "Macropus rufogriseus",
-        description:
-          "สัตว์เลี้ยงลูกด้วยน้ำนมที่มีหน้าท้องในเพศเมีย เป็นสัตว์พื้นเมืองในทวีปออสเตรเลีย และเกาะนิวกินี ลักษณะคล้ายจิงโจ้ที่มีขนาดเล็กแต่มีข้อแตกต่าง เช่น เท้าที่มีความยาวมากกว่า และขาที่สั้นกว่า สีขนจะมีสีสว่างและมีสีสันมากกว่า นอกจากนี้ฟันกรามของวัลลาบีจะมี ลักษณะที่เป็นแถวแบนและมีการสลัดฟันกรามน้อยเนื่องจากวัลลาบีไม่ใช้ฟันในการตัดอาหารมากเท่าจิงโจ้  อาหารหลักคือ หญ้า ใบไม้ ผลไม้ และสามารถดื่มน้ำทะเลได้อีกด้วย มักอาศัยในบริเวณที่มีหินและอยู่ใกล้เเหล่งน้ำ เข้าสู่ฤดูผสมพันธุ์ช่วงมกราคม-กุมภาพันธ์ ตั้งท้องประมาณ 28 วัน เมื่อคลอดลูกวัลลาบีจะปีนเข้าไปในหน้าท้องแม่ น้ำหนักเมื่อโตเต็มที่อยู่ที่ 20 กิโลกรัม สูงประมาณ 104 เซนติเมตร",
-        taxonomy: "60366bb55eb22332cc9c8045",
-        animalTypeId: "603677925eb22332cc9c8056",
-        __v: 0,
-        complete: true,
-        completeImagePath: [
-          "img-1616171184793.jpg", //1
-          "img-1616171184842.jpg",
-          "img-1616171184878.jpg",
-          "img-1616171184913.jpg",
-          "img-1616171184934.jpg", //5
-          "img-1616171184966.jpg",
-          "img-1616171185001.jpg",
-          "img-1616171185026.jpg",
-          "img-1616171185077.jpg",
-          "img-1616171185096.jpg", //10
-          "img-1616171185132.jpg",
-          "img-1616171185154.jpg",
-          "img-1616171185199.jpg",
-          "img-1616171185230.jpg",
-          "img-1616171185249.jpg", //15
-          "img-1616171185276.jpg",
-        ],
-        bone: {
-          _id: "6040e71c46fdea04372083ac",
-          axial: {
-            vertebralColumn: {
-              cervicalVertebrae: true,
-              toracicVertebrae: true,
-              ribsCage: true,
-              sternum: true,
-              lumbarVertebrae: true,
-              sarcum: true,
-              coccygeal: true,
-            },
-            skull: true,
-            mandible: true,
-          },
-          appendicular: {
-            forelimb: {
-              scapular: true,
-              clavicle: true,
-              humerus: true,
-              radias: true,
-              ulnar: true,
-              carpus: true,
-              metacapus: true,
-              phalangs: true,
-              sesamoid: false,
-            },
-            hindlimb: {
-              pelvic: true,
-              femur: true,
-              patella: false,
-              fabella: false,
-              tibia: true,
-              fibular: true,
-              tarsus: true,
-              metatarsus: true,
-              phalangs: true,
-              sesamoid: false,
-            },
-          },
-          no: "14",
-          species: "Wallaby",
-          __v: 0,
-        },
-        data: [],
-        platform: "Web",
-      },
-
-      GetFromIdTest: { //ข้อมูลกรณีที่เคย Tag แล้ว 
-        _id: "604c6037cfc890051804f6d8",
-        animal: {
-          _id: "6048e482c9eb27085c2bd571",
-          completeImagePath: [
-            "img-1615392107089.jpg",
-            "img-1615392107092.png",
-            "img-1615392107096.png",
-          ],
-          partType: ["6048e55ec9eb27085c2bd572", "6048e57dc9eb27085c2bd573"],
-          thaiName: "สุนัขทดสอบ สอง",
-          englishName: "Dog Tesing 2",
-          scienceName: "Dog Tesing 2",
-          technicalTerm: "Dog Tesing 2",
-          description: "ทดสอบระบบ",
-          taxonomy: null,
-          animalTypeId: "6033be30149207022c774778",
-          partId: null,
-          __v: 0,
-          complete: true,
-          boneId: "6040e71c46fdea043720839f",
-        },
-        bone: {
-          _id: "6040e71c46fdea043720839f",
-          axial: {
-            vertebralColumn: {
-              cervicalVertebrae: true,
-              toracicVertebrae: true,
-              ribsCage: true,
-              sternum: true,
-              lumbarVertebrae: true,
-              sarcum: true,
-              coccygeal: false,
-            },
-            skull: true,
-            mandible: true,
-          },
-          appendicular: {
-            forelimb: {
-              scapular: true,
-              clavicle: false,
-              humerus: true,
-              radias: true,
-              ulnar: true,
-              carpus: true,
-              metacapus: true,
-              phalangs: true,
-              sesamoid: false,
-            },
-            hindlimb: {
-              pelvic: true,
-              femur: true,
-              patella: false,
-              fabella: false,
-              tibia: true,
-              fibular: true,
-              tarsus: true,
-              metatarsus: true,
-              phalangs: true,
-              sesamoid: false,
-            },
-          },
-          no: "1",
-          species: "Domestic Dog",
-          __v: 0,
-        },
-        data: [
-          {
-            imageURL: "http://localhost:3000/img/animals/img-1615392107089.jpg",
-            _id: "604cd2bf2b31791099ec827e",
-            imageName: "img-1615392107089.jpg",
-            coordinator: [
-              {
-                _id: "604c6037cfc890051804f6db",
-                title: "mandible",
-                positionX: 111,
-                positionY: 222,
-                lineStartX: 333,
-                lineStartY: 444,
-                lineToX: 555,
-                lineToY: 666,
-                description: "test position",
-              },
-              {
-                _id: "604cd2bf2b31791099ec8281",
-                title: "mandible1",
-                positionX: 1,
-                positionY: 1,
-                lineStartX: 1,
-                lineStartY: 1,
-                lineToX: 1,
-                lineToY: 1,
-                description: "test add new position",
-              },
-              {
-                _id: "604cd2bf2b31791099ec8280",
-                title: "skull",
-                positionX: 2002,
-                positionY: 2002,
-                lineStartX: 2002,
-                lineStartY: 2002,
-                lineToX: 2002,
-                lineToY: 2002,
-                description: "test Update position มั้ง",
-              },
-              {
-                _id: "604f815b61b3b4065a042b1a",
-                title: "mandible1",
-                positionX: 1001,
-                positionY: 1001,
-                lineStartX: 1001,
-                lineStartY: 1001,
-                lineToX: 1001,
-                lineToY: 1001,
-                description: "test add new position มั้ง",
-              },
-            ],
-          },
-          {
-            imageURL: "http://localhost:3000/img/animals/img-1615392107092.png",
-            _id: "604cd2bf2b31791099ec8282",
-            imageName: "img-1615392107092.png",
-            coordinator: [
-              {
-                _id: "604c6037cfc890051804f6dd",
-                title: "skull",
-                positionX: 111,
-                positionY: 222,
-                lineStartX: 333,
-                lineStartY: 444,
-                lineToX: 555,
-                lineToY: 666,
-                description: "test position",
-              },
-            ],
-          },
-        ],
-        __v: 0,
-        platform: "Web",
-      },
+      animalGet: [],
       aaa:[],
     };
   },
@@ -467,7 +190,7 @@ export default {
       } catch (error) {
         console.log("don't have bone!")
         // console.log(error)
-        window.alert("please insert bone!");
+        window.alert("please insert bone data!");
       }
 
       try {
@@ -476,7 +199,7 @@ export default {
       } catch (error) {
         console.log("don't have completeImagePath!")
         // console.log(error)
-        window.alert("please insert completeImage!");
+        window.alert("please insert image data!");
       }
 
       
@@ -511,12 +234,11 @@ export default {
           await bone &&
           await boneL == 0
         ) {
-          console.log("please insert bone data!");
+          console.log("not found bone data!");
         } else if(
           await comImgL == 0
           ){ //ไม่มี complete img path
-          console.log("please insert completeImage data!");
-          window.alert("please insert completeImage!");
+          console.log("not found completeImage data!");
         }
       
       
